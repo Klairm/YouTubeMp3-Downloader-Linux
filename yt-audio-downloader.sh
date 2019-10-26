aa8103bab26a8cb1f454f7bb95020c6066d2929e
Jacob Hrbek <werifgx@gmail.com>
2019-10-26 07:56:41 +0200

    There is no need to use root here

--- yt-audio-downloader.sh
+++ yt-audio-downloader.sh
@@ -3,6 +3,4 @@
 printf '%s\n' "Input the URL of your expected youtube video to extract audio from:"
 read url
 
-sudo youtube-dl --extract-audio --audio-format mp3 -l $url
-
-
+youtube-dl --extract-audio --audio-format mp3 -l $url

9284b36380d4bab3cea20e0fb532aee9eeff5116
Ivan Di Scipio <40839469+Klairm@users.noreply.github.com>
2019-10-29 19:42:43 +0100

    Update yt-audio-downloader.sh

--- yt-audio-downloader.sh
+++ yt-audio-downloader.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-youtube-dl --extract-audio --audio-format mp3 -l $@1
-
+EXPECTED_ARGS=1
+if [ $# -ne $EXPECTED_ARGS ]
+   then
+   echo "Usage: ./yt-audio-downloader.sh <URL>"
+   exit
+   else
+   youtube-dl --extract-audio --audio-format mp3 -l $@1
+fi
